504cc95eab3e3052124c45ebd5a73f38aa85ac03
aman-senpai <jemscollin1@gmail.com>
2023-05-30 19:08:00 +0530

    Finished project

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import "@styles/globals.css";
+
+import Nav from "@components/nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+    title: "Promptopia",
+    description: "A place to find inspiration for your next prompt",
+};
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang="en">
+            <body>
+                <Provider>
+                    <div className="main">
+                        <div className="gradient" />
+                    </div>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    );
+};
+
+export default RootLayout;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const { mongo, default: mongoose } = require('mongoose')
+
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
